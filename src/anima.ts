eb5b22766d6135be1d3e54e12866399df801e932
Jose Cerqueira <josecerqueira@protonmail.com>
2025-01-03 03:40:31 +0000

    :sparkles: feat: add owners

--- src/anima.ts
+++ src/anima.ts
@@ -3,8 +3,15 @@ import {
   ANIMA_WEAPON_TYPE,
   AVATAR_TYPE,
 } from './constants';
-
-import { log, graphQLClient, writeFile } from './utils';
+import { chunkArray } from '@polymedia/suitcase-core';
+import {
+  log,
+  graphQLClient,
+  writeFile,
+  readFile,
+  rpcClient,
+  sleep,
+} from './utils';
 
 import { queryNFTObjects } from './query';
 import { pathOr } from 'ramda';
@@ -25,8 +32,6 @@ export const getNFTObjects = async ({
     variables: { type: objectType, first, after },
   });
 
-  log(result);
-
   return {
     pageInfo: {
       endCursor: pathOr(
@@ -42,40 +47,97 @@ export const getNFTObjects = async ({
     },
     nfts: pathOr([], ['data', 'objects', 'nodes'], result).map((node) => ({
       owner: pathOr('', ['owner', 'parent', 'address'], node),
-      content: pathOr('', ['asMoveObject', 'contents', 'json'], node),
+      id: pathOr('', ['asMoveObject', 'contents', 'json', 'id'], node),
     })),
   };
 };
 
 (async () => {
-  let after = null;
-  let results = [];
-  let objects = await getNFTObjects({
-    objectType:
-      '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet',
-    after,
-    first: 50,
-  });
+  const file = await readFile(`${__dirname}/../data/rootlets-owners.json`);
+  const rootlets: { id: string; kiosk: string }[] = JSON.parse(file.toString());
 
-  results.push(...objects.nfts);
-  after = objects.pageInfo.endCursor;
+  log(rootlets);
 
-  while (after) {
-    objects = await getNFTObjects({
-      objectType:
-        '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet',
-      after,
-      first: 50,
+  const rootletsMap = rootlets.reduce((acc, r) => {
+    if (acc[r.kiosk]) {
+      acc[r.kiosk].push(r.id);
+    } else {
+      acc[r.kiosk] = [r.id];
+    }
+    return acc;
+  }, {} as any);
+
+  const chunks = chunkArray(Object.keys(rootletsMap), 50);
+
+  const results = [];
+
+  for (const owners of chunks) {
+    const kiosk = await rpcClient.multiGetObjects({
+      ids: owners,
+      options: {
+        showContent: true,
+      },
     });
 
-    results.push(...objects.nfts);
-    after = objects.pageInfo.endCursor;
+    results.push(...kiosk);
+
+    await sleep(1000);
   }
 
-  log(results.length);
+  const rootletsWithOwners = results.reduce((acc, r, i) => {
+    acc[r.data?.objectId!] = {
+      ...rootletsMap[r.data?.objectId!],
+      owner: (r.data as any)?.content?.fields?.owner,
+    };
+    return acc;
+  }, {} as any);
 
   await writeFile(
-    `${__dirname}/../data/rootlets.json`,
-    JSON.stringify(results, null, 2)
+    `${__dirname}/../data/rootlets-owners.json`,
+    JSON.stringify(rootletsWithOwners, null, 2)
   );
+
+  // log(kiosk);
+
+  // const kioskOwner = await rpcClient.getObject({
+  //   id: '0xc550892c851457143230fd43acdd3e4cdb816d9cab701ba4cb1f03f0271e9773',
+  //   options: {
+  //     showContent: true,
+  //   },
+  // });
+
+  // log(kioskOwner);
 })();
+
+// (async () => {
+//   let after = null;
+//   let results = [];
+//   let objects = await getNFTObjects({
+//     objectType:
+//       '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet',
+//     after,
+//     first: 50,
+//   });
+
+//   results.push(...objects.nfts);
+//   after = objects.pageInfo.endCursor;
+
+//   while (after) {
+//     objects = await getNFTObjects({
+//       objectType:
+//         '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet',
+//       after,
+//       first: 50,
+//     });
+
+//     results.push(...objects.nfts);
+//     after = objects.pageInfo.endCursor;
+//   }
+
+//   log(results.length);
+
+//   await writeFile(
+//     `${__dirname}/../data/rootlets-field-ids.json`,
+//     JSON.stringify(results, null, 2)
+//   );
+// })();
