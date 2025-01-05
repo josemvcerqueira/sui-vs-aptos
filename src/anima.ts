4aa1c93d8bd42589771234186f4e612b7fa92a5e
Jose Cerqueira <josecerqueira@protonmail.com>
2025-01-05 18:55:18 +0000

    :sparkles: chore: add example of rootlet reveal

--- src/anima.ts
+++ src/anima.ts
@@ -12,6 +12,7 @@ import {
   rpcClient,
   sleep,
 } from './utils';
+import { Transaction } from '@mysten/sui/transactions';
 
 import { queryNFTObjects } from './query';
 import { pathOr } from 'ramda';
@@ -53,60 +54,68 @@ export const getNFTObjects = async ({
 };
 
 (async () => {
-  const file = await readFile(`${__dirname}/../data/rootlets-owners.json`);
-  const rootlets: { id: string; kiosk: string }[] = JSON.parse(file.toString());
+  const rootletId =
+    '0xefe734942dc74e220761655eee40571276387390f65449c5980dbe17005c3c56';
 
-  log(rootlets);
+  const kioskId =
+    '0xe01abdd6046ffe32058fea11e3ca6944271c5ae0da8cd633c6e2d8cd5a4babd4';
 
-  const rootletsMap = rootlets.reduce((acc, r) => {
-    if (acc[r.kiosk]) {
-      acc[r.kiosk].push(r.id);
-    } else {
-      acc[r.kiosk] = [r.id];
-    }
-    return acc;
-  }, {} as any);
+  const rootletType =
+    '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet';
 
-  const chunks = chunkArray(Object.keys(rootletsMap), 50);
+  const rootletImgId =
+    '0xfdb524be4a187112299445024a25fbc799e9499fc55ecbc878d3345e82468f12';
 
-  const results = [];
+  const packageId =
+    '0xbe7741c72669f1552d0912a4bc5cdadb5856bcb970350613df9b4362e4855dc5';
 
-  for (const owners of chunks) {
-    const kiosk = await rpcClient.multiGetObjects({
-      ids: owners,
-      options: {
-        showContent: true,
-      },
-    });
+  const saleV2 =
+    '0x2ce5c11867a713bcd0a60620bb15964769f10bbeb86133a271cffcae404ab32e';
 
-    results.push(...kiosk);
+  const rootletOwner =
+    '0x4182d1e45353a76b862da8c4aa601fa105de02e92459cce30bc9170123130251';
 
-    await sleep(1000);
-  }
+  const PERSONAL_KIOSK_PACKAGE =
+    '0x0cb4bcc0560340eb1a1b929cabe56b33fc6449820ec8c1980d69bb98b649b802';
 
-  const rootletsWithOwners = results.reduce((acc, r, i) => {
-    acc[r.data?.objectId!] = {
-      ...rootletsMap[r.data?.objectId!],
-      owner: (r.data as any)?.content?.fields?.owner,
-    };
-    return acc;
-  }, {} as any);
+  const personalCapId =
+    '0x55021c50b82bd1b75239555d990fc82d5e99b8b494284eb1723701fc95e1502e';
 
-  await writeFile(
-    `${__dirname}/../data/rootlets-owners.json`,
-    JSON.stringify(rootletsWithOwners, null, 2)
+  const tx = new Transaction();
+
+  const [kioskOwnerCap, perosnalBorrow] = tx.moveCall({
+    target: `${PERSONAL_KIOSK_PACKAGE}::personal_kiosk::borrow_val`,
+    arguments: [tx.object(personalCapId)],
+  });
+
+  const [nft, nftBorrow] = tx.moveCall({
+    target: `0x2::kiosk::borrow_val`,
+    arguments: [tx.object(kioskId), kioskOwnerCap, tx.pure.id(rootletId)],
+    typeArguments: [rootletType],
+  });
+
+  tx.moveCall({
+    target: `${packageId}::rootlet::reveal_v2`,
+    arguments: [nft, tx.object(saleV2), tx.object(rootletImgId)],
+  });
+
+  tx.moveCall({
+    target: `0x2::kiosk::return_val`,
+    arguments: [tx.object(kioskId), nft, nftBorrow],
+    typeArguments: [rootletType],
+  });
+
+  tx.moveCall({
+    target: `${PERSONAL_KIOSK_PACKAGE}::personal_kiosk::return_val`,
+    arguments: [tx.object(personalCapId), kioskOwnerCap, perosnalBorrow],
+  });
+
+  console.log(
+    await rpcClient.devInspectTransactionBlock({
+      transactionBlock: tx,
+      sender: rootletOwner,
+    })
   );
-
-  // log(kiosk);
-
-  // const kioskOwner = await rpcClient.getObject({
-  //   id: '0xc550892c851457143230fd43acdd3e4cdb816d9cab701ba4cb1f03f0271e9773',
-  //   options: {
-  //     showContent: true,
-  //   },
-  // });
-
-  // log(kioskOwner);
 })();
 
 // (async () => {
