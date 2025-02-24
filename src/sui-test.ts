d67ef5db35f4e720dbf8b3aa36242cb63441ae58
Jose Cerqueira <josecerqueira@protonmail.com>
2025-02-24 23:21:47 +0000

    :sparkles: feat: add sui perf results

---
+++ src/sui-test.ts
@@ -0,0 +1,76 @@
+import { keypair, sleep, suiClient, log, writeFile } from './utils';
+import { Transaction } from '@mysten/sui/transactions';
+
+const COUNTER_CONTRACT_ADDRESS =
+  '0xdd4dbcc34a8c05c2367d109119452920cb6776501e10d98bbd1f8ecb5cd97686';
+const COUNTER_SHARED_OBJECT = {
+  objectId:
+    '0x2388b7872b85430d702a453028d4896be8a4acee2ec6b2e41b779fa12a329004',
+  initialVersion: '477215908',
+  mutable: true,
+};
+
+const TOTAL_TX = 10;
+
+(async () => {
+  const gasPrice = await suiClient.getReferenceGasPrice();
+
+  const results = [];
+
+  for (let i = 0; i < TOTAL_TX; i++) {
+    const tx = new Transaction();
+    tx.setSender(keypair.toSuiAddress());
+    tx.setGasPrice(Math.floor(Number(gasPrice) * 1.1));
+
+    tx.setGasBudget(5_000_000);
+
+    tx.moveCall({
+      target: `${COUNTER_CONTRACT_ADDRESS}::counter::increment`,
+      arguments: [
+        tx.sharedObjectRef({
+          objectId: COUNTER_SHARED_OBJECT.objectId,
+          initialSharedVersion: COUNTER_SHARED_OBJECT.initialVersion,
+          mutable: COUNTER_SHARED_OBJECT.mutable,
+        }),
+      ],
+    });
+
+    const buildStartTime = performance.now();
+    const bytes = await tx.build({ client: suiClient });
+
+    const startTime = performance.now();
+
+    await suiClient.signAndExecuteTransaction({
+      signer: keypair,
+      transaction: bytes,
+    });
+
+    const endTime = performance.now();
+
+    const buildLatency = (startTime - buildStartTime) / 1000;
+    const latency = (endTime - startTime) / 1000;
+
+    log(`Build latency: ${buildLatency}; E2E latency: ${latency} s`);
+
+    results.push({
+      buildLatency,
+      latency,
+    });
+
+    await sleep(2_000);
+  }
+
+  const summary = {
+    results,
+    averageBuildLatency:
+      results.reduce((acc, curr) => acc + curr.buildLatency, 0) /
+      results.length,
+    averageLatency:
+      results.reduce((acc, curr) => acc + curr.latency, 0) / results.length,
+  };
+
+  await writeFile(
+    `${__dirname}/../sui-results.json`,
+    JSON.stringify(summary, null, 2)
+  );
+})();
--- src/utils.ts
+++ src/utils.ts
@@ -1,46 +1,28 @@
-import {
-  SuiClient,
-  SuiObjectResponse,
-  getFullnodeUrl,
-} from '@mysten/sui/client';
-import { SuiGraphQLClient } from '@mysten/sui/graphql';
+import { SuiClient, getFullnodeUrl } from '@mysten/sui/client';
 import util from 'util';
-import { pathOr } from 'ramda';
 import * as fs from 'fs';
+import dotenv from 'dotenv';
+import invariant from 'tiny-invariant';
+import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
+
+dotenv.config();
+
+invariant(process.env.SUI_PRIVATE_KEY, 'Private key missing');
+
+export const keypair = Ed25519Keypair.fromSecretKey(
+  Uint8Array.from(Buffer.from(process.env.SUI_PRIVATE_KEY, 'base64')).slice(1)
+);
 
 export const log = (x: any) => console.log(util.inspect(x, false, null, true));
 
-export const suiFullNodeUrl =
-  process.env.SUI_FULL_NODE_URL || getFullnodeUrl('mainnet');
+export const suiFullNodeUrl = getFullnodeUrl('mainnet');
 
-export const rpcClient = new SuiClient({
+export const suiClient = new SuiClient({
   url: suiFullNodeUrl,
 });
 
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
-
-export const parseSuiPlayRegistry = (obj: SuiObjectResponse) => {
-  return {
-    objectId: pathOr('', ['data', 'objectId'], obj),
-    type: pathOr('', ['data', 'content', 'type'], obj),
-    version: pathOr('', ['data', 'version'], obj),
-    exaltedMinted: pathOr(
-      '',
-      ['data', 'content', 'fields', 'exalted_minted'],
-      obj
-    ),
-    mythicsMinted: pathOr(
-      '',
-      ['data', 'content', 'fields', 'mythics_minted'],
-      obj
-    ),
-  };
-};
-
-export const graphQLClient = new SuiGraphQLClient({
-  url: 'https://sui-mainnet.mystenlabs.com/graphql',
-});
 
 export const readFile = util.promisify(fs.readFile);
 
